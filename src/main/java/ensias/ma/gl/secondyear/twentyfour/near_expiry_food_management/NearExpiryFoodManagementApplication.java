467d926463351d22f8afea159e1bbcb52706bfc9
mehdicharife <mehdicharife@yahoo.com>
2024-09-20 22:03:28 +0100

    initial commit

---
+++ src/main/java/ensias/ma/gl/secondyear/twentyfour/near_expiry_food_management/NearExpiryFoodManagementApplication.java
@@ -0,0 +1,13 @@
+package ensias.ma.gl.secondyear.twentyfour.near_expiry_food_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearExpiryFoodManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearExpiryFoodManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ensias/ma/gl/secondyear/twentyfour/near_expiry_food_management/NearExpiryFoodManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ensias.ma.gl.secondyear.twentyfour.near_expiry_food_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearExpiryFoodManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
